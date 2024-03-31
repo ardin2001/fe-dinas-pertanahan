3dbe2fcd300b4794d03d18b13b5a9d909c636e0c
IrfanM66 <civilwar776677@gmail.com>
2024-03-31 23:13:25 +0700

    update icon

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,38 +1,54 @@
-import logo from '../assets/logo.png'
-import { useEffect,useRef } from 'react'
+import logo from "../assets/logo.png";
+import { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
 function App() {
   const navigate = useNavigate();
-  const handlerLogin = (event) =>{
-    event.preventDefault()
-    navigate('/dashboard')
-  }
+  const handlerLogin = (event) => {
+    event.preventDefault();
+    navigate("/dashboard");
+  };
   const usernameRef = useRef(null);
-  useEffect(() =>{
+  useEffect(() => {
     usernameRef.current.focus();
-  },[])
+  }, []);
 
-  return (    
-    <div className='grid h-screen bg-gray-200'>
-      <div className='grid grid-flow-col grid-cols-2 m-12 drop-shadow-custom-login'>
-        <div className='grid justify-items-center items-center bg-green-500 border-y-2 border-white border-l-2'>
-          <div className='grid justify-items-center w-10/12 gap-5'>
-            <img src={logo} alt="" className='pr-12' />
-            <p className='text-center'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus amet ullam eveniet quasi quidem quae nobis in ratione corporis fuga?</p>
+  return (
+    <div className="grid h-screen bg-gray-200">
+      <div className="grid grid-flow-col grid-cols-2 m-12 drop-shadow-custom-login">
+        <div className="grid justify-items-center items-center bg-slate-500 border-y-2 border-white border-l-2 ">
+          <div className="grid justify-items-center w-10/12 gap-5">
+            <img src={logo} alt="" className="pr-12" />
           </div>
         </div>
-        <div className='grid bg-white items-center justify-items-center border-y-2 border-green-500 border-r-2'>
-          <form onSubmit={handlerLogin} className='w-7/12 grid gap-4 px-10 py-3 rounded-md'>
-            <h2 className='text-3xl font-semibold text-center'>Login Admin</h2>
-            <input ref={usernameRef} type="text" placeholder='username' className='h-8 py-2 outline-none border-green-500 border-b-2' />
-            <input type="password" placeholder='password' className='h-8 py-2 outline-none border-green-500 border-b-2' />
-            <button type='submit' className='bg-green-500 w-1/2 justify-self-center h-7 font-semibold'>Login</button>
+        <div className="grid bg-white items-center justify-items-center border-y-2 border-slate-500 border-r-2">
+          <form
+            onSubmit={handlerLogin}
+            className="w-7/12 grid gap-4 px-10 py-3 rounded-md"
+          >
+            <h2 className="text-3xl font-semibold text-center">Login Admin</h2>
+            <input
+              ref={usernameRef}
+              type="text"
+              placeholder="username"
+              className="h-8 py-2 outline-none border-slate-500 border-b-2"
+            />
+            <input
+              type="password"
+              placeholder="password"
+              className="h-8 py-2 outline-none border-slate-500 border-b-2"
+            />
+            <button
+              type="submit"
+              className="bg-slate-500 w-1/2 justify-self-center h-7 font-semibold"
+            >
+              Login
+            </button>
           </form>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,41 +1,61 @@
 import logo from "../assets/logo.png";
-import { AiOutlineLogout } from "react-icons/ai";
-import { FaRegEnvelope } from "react-icons/fa";
-import { FaHome } from "react-icons/fa";
-import { FaUser } from "react-icons/fa";
-import { FaClipboardList } from "react-icons/fa";
 import { useState } from "react";
+import { Home, Logout, Message, Paper, User } from "react-iconly";
 
 const DashboardPage = () => {
   const [current, setCurrent] = useState(null);
-  const handlerCurrent = (menu) =>{
-    setCurrent(menu)
-  }
+  const handlerCurrent = (menu) => {
+    setCurrent(menu);
+  };
   return (
     <main className="grid grid-cols-5 h-screen gap-8 bg-gray-200">
-      <div className="sidebar col-span-1 grid grid-rows-8 bg-white drop-shadow-custom">
+      <div className="sidebar col-span-1 grid grid-rows-8 bg-white drop-shadow-custom font-sans">
         <div className="title row-span-1 grid grid-cols-8 items-center mx-6">
           <img src={logo} alt="" className=" col-span-2" />
-          <h3 className="col-start-3 col-end-9 font-bold text-lg justify-self-end tracking-wide">
+          <h3 className="col-start-3 col-end-9 font-bold text-base justify-self-center tracking-wide">
             ATR/BPN JEMBER
           </h3>
         </div>
         <div className="menu row-start-3 row-end-8 mx-6">
           <ul>
-            <li onClick={() => handlerCurrent('beranda')} className={`${current == 'beranda' ? 'bg-slate-500 rounded-lg text-white' : null} hover:cursor-pointer py-3 flex gap-2 items-center font-semibold text-lg pl-3`}>
-              <FaHome />
+            <li
+              onClick={() => handlerCurrent("beranda")}
+              className={`${
+                current == "beranda"
+                  ? "bg-slate-500 rounded-lg text-white"
+                  : null
+              } hover:cursor-pointer py-3 flex gap-3 items-center font-medium text-base pl-3`}
+            >
+              <Home />
               <p>Beranda</p>
             </li>
-            <li onClick={() => handlerCurrent('persuratan')} className={`${current == 'persuratan' ? 'bg-slate-500 rounded-lg text-white' : null} hover:cursor-pointer py-3 flex gap-2 items-center font-semibold text-lg pl-3`}>
-              <FaRegEnvelope />
+            <li
+              onClick={() => handlerCurrent("persuratan")}
+              className={`${
+                current == "persuratan"
+                  ? "bg-slate-500 rounded-lg text-white"
+                  : null
+              } hover:cursor-pointer py-3 flex gap-3 items-center font-medium text-base pl-3`}
+            >
+              <Message />
               <p>Persuratan</p>
             </li>
-            <li onClick={() => handlerCurrent('rekap')} className={`${current == 'rekap' ? 'bg-slate-500 rounded-lg text-white' : null} hover:cursor-pointer py-3 flex gap-2 items-center font-semibold text-lg pl-3`}>
-              <FaClipboardList />
+            <li
+              onClick={() => handlerCurrent("rekap")}
+              className={`${
+                current == "rekap" ? "bg-slate-500 rounded-lg text-white" : null
+              } hover:cursor-pointer py-3 flex gap-3 items-center font-medium text-base pl-3`}
+            >
+              <Paper />
               <p>Rekap Surat</p>
             </li>
-            <li onClick={() => handlerCurrent('user')} className={`${current == 'user' ? 'bg-slate-500 rounded-lg text-white' : null} hover:cursor-pointer py-3 flex gap-2 items-center font-semibold text-lg pl-3`}>
-              <FaUser />
+            <li
+              onClick={() => handlerCurrent("user")}
+              className={`${
+                current == "user" ? "bg-slate-500 rounded-lg text-white" : null
+              } hover:cursor-pointer py-3 flex gap-3 items-center font-medium text-base pl-3`}
+            >
+              <User />
               <p>Manajemen User</p>
             </li>
           </ul>
@@ -43,17 +63,20 @@ const DashboardPage = () => {
         <div className="account row-span-8 flex justify-between items-center mx-8">
           <div className="status">
             <h4 className="font-bold text-base">ADMIN ATR/BPN</h4>
-            <p className="text-sm">usertest123@gmail.com</p>
+            <p className="text-sm">admin123@gmail.com</p>
           </div>
           <div className="logout">
-            <AiOutlineLogout size="1.5em" />
+            <Logout size="1.5em" />
           </div>
         </div>
       </div>
       <div className="content col-start-2 col-end-6 w-97/100">
         <div className="navbar flex justify-between pt-5">
-          <h2 className="font-bold text-2xl">Dashboard</h2>
-          <select id="month" className="pr-6 font-normal outline-none">
+          <h2 className="font-bold text-xl">Dashboard</h2>
+          <select
+            id="month"
+            className="pr-6 font-normal outline-none rounded-md text-sm p-1"
+          >
             <option value="volvo">Bulan Ini</option>
             <option value="saab">April</option>
             <option value="opel">Mei</option>
@@ -61,10 +84,10 @@ const DashboardPage = () => {
           </select>
         </div>
         <div className="rekap grid gap-10 grid-flow-col grid-cols-4 mt-8">
-            <div className="masuk h-32 bg-white rounded-lg drop-shadow-custom"></div>
-            <div className="disposisi h-32 bg-white rounded-lg drop-shadow-custom"></div>
-            <div className="belum-ditindaklanjuti h-32 bg-white rounded-lg drop-shadow-custom"></div>
-            <div className="sudah-ditindaklanjuti h-32 bg-white rounded-lg drop-shadow-custom"></div>
+          <div className="masuk h-32 bg-white rounded-lg drop-shadow-custom"></div>
+          <div className="disposisi h-32 bg-white rounded-lg drop-shadow-custom"></div>
+          <div className="belum-ditindaklanjuti h-32 bg-white rounded-lg drop-shadow-custom"></div>
+          <div className="sudah-ditindaklanjuti h-32 bg-white rounded-lg drop-shadow-custom"></div>
         </div>
         <div className="rekap mt-8 bg-white h-3/5 rounded-lg drop-shadow-custom"></div>
       </div>
