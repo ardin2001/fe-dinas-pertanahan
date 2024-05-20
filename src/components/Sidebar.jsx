f9ae9571f2a6339c8f184f474119ce1c04200bfa
IrfanM66 <civilwar776677@gmail.com>
2024-05-20 08:55:22 +0700

    updated alert

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import logo from "./../assets/logo.png";
+import logo from "../assets/logo.png";
 import { useState } from "react";
 import { GoHome } from "react-icons/go";
 import { SlEnvolopeLetter } from "react-icons/sl";
