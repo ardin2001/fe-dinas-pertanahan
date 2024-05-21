2a56f91800d5cfaaebdf68986fb84489a4b76bad
IrfanM66 <civilwar776677@gmail.com>
2024-05-21 13:50:56 +0700

    updated features

--- src/components/modal/notification.jsx
+++ src/components/modal/notification.jsx
@@ -2,7 +2,7 @@ export default function ModalNotification({notif}) {
     if (!notif) return null
     return (
         <div className="notification w-52 h-96 bg-white shadow-xl absolute -translate-x-full z-50 rounded-xl">
-            <p className="m-3">Notifikasi</p>
+            <p className="m-3">Notifikasi Surat</p>
         </div>
     )
 }
