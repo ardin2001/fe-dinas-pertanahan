1c5b31a94b926e5ac0f79ccbfcc797e2b78c1571
IrfanM66 <civilwar776677@gmail.com>
2024-05-23 13:34:27 +0700

    updated url

--- src/utils/FetchRekapSurat.js
+++ src/utils/FetchRekapSurat.js
@@ -1,28 +1,13 @@
 const url = "https://api.persuratankantah.xyz/api";
 
-const GetRekapSurat = async () => {
-  const response = await fetch(url + "/show-rekap", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + sessionStorage.getItem("token"),
-    },
-  });
-  const json = await response.json();
-  return json;
-};
-
-const getShowFileRekap = async (id, type) => {
-  const response = await fetch(url + "/show-file/" + id + "/" + type, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + sessionStorage.getItem("token"),
-    },
-  });
-  const blob = await response.blob();
-  const objectUrl = window.URL.createObjectURL(blob);
-  return objectUrl;
-};
-
-export { GetRekapSurat, getShowFileRekap };
+export const GetRekapSurat = async() => {
+    const response = await fetch(url+'/show-rekap',{
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + sessionStorage.getItem('token'),
+        },
+    });
+    const json = await response.json();
+    return json
+}
