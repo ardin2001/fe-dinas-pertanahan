97ca2abb65bdbb95b4016fdf3ba28df9edc3e86b
IrfanM66 <civilwar776677@gmail.com>
2024-05-27 11:09:39 +0700

    updated features

--- src/components/modal/manajemen-user/TambahUser.jsx
+++ src/components/modal/manajemen-user/TambahUser.jsx
@@ -1,29 +1,35 @@
 import { AiOutlineCloseSquare } from "react-icons/ai";
-import { PostManagemenUser,GetDetailMnagemenUser } from "../../../utils/FetchmanagemenUser";
+import {
+  PostManagemenUser,
+  GetDetailMnagemenUser,
+} from "../../../utils/FetchmanagemenUser";
 const ModalTambah = (props) => {
-  const { modal, HandlerTambah,setUsers } = props;
+  const { modal, HandlerTambah, setUsers } = props;
   const HandlerSubmit = async (event) => {
     event.preventDefault();
     const data = {
       nama: event.target.name.value,
       email: event.target.email.value,
       password: event.target.password.value,
-      type:event.target.type.value
-    }
-    const response = await PostManagemenUser(data)
-    if(response.status === true){
-      const {data} = await GetDetailMnagemenUser(response.userId)
-      setUsers((prev) => [...prev,data]);
-      HandlerTambah({status:response.status})
-    }else{
-      HandlerTambah({status:false})
+      type: event.target.type.value,
+    };
+    const response = await PostManagemenUser(data);
+    if (response.status === true) {
+      const { data } = await GetDetailMnagemenUser(response.userId);
+      setUsers((prev) => [...prev, data]);
+      HandlerTambah({ status: response.status });
+    } else {
+      HandlerTambah({ status: false });
     }
   };
   if (!modal) {
     return null;
   }
   return (
-    <form onSubmit={HandlerSubmit} className="fixed bg-white py-4 border-solid rounded-lg drop-shadow-custom z-50 inset-x-38/100 inset-y-1/10">
+    <form
+      onSubmit={HandlerSubmit}
+      className="fixed bg-white py-4 border-solid rounded-lg drop-shadow-custom z-50 inset-x-38/100 inset-y-1/10"
+    >
       <div className="header flex justify-between py-4 w-10/12 m-auto items-center">
         <h3 className="font-semibold text-xl text-custom">Tambah User</h3>
         <AiOutlineCloseSquare
@@ -78,10 +84,7 @@ const ModalTambah = (props) => {
           />
         </div>
         <div className="type grid gap-1">
-          <label
-            htmlFor="type"
-            className="text-custom text-base font-semibold"
-          >
+          <label htmlFor="type" className="text-custom text-base font-semibold">
             Role
           </label>
           <select
@@ -89,25 +92,44 @@ const ModalTambah = (props) => {
             className="outline-none border-2 border-quaternary w-full py-2.5 px-3 text-sm text-custom rounded-lg"
             name="type"
           >
-            <option className="font-normal" value="">
-              Jenis Role
+            <option className="font-normal" value="0">
+              Admin
             </option>
             <option className="font-normal" value="1">
-              Mid
+              Kepala Kantor
             </option>
             <option className="font-normal" value="2">
-              Jungler
+              Kasubag. TU
             </option>
             <option className="font-normal" value="3">
-              Exp
+              Seksi Penetapan Hak & Pendaftaran
+            </option>
+            <option className="font-normal" value="4">
+              Seksi Survei & Pemetaan
+            </option>
+            <option className="font-normal" value="5">
+              Seksi Penataan & Pemberdayaan
+            </option>
+            <option className="font-normal" value="6">
+              Seksi Pengadaan Tanah & Pengembangan
+            </option>
+            <option className="font-normal" value="7">
+              Seksi Pengendalian & Penanganan Sengketa
             </option>
           </select>
         </div>
         <div className="button grid gap-8 grid-flow-col text-white font-semibold text-center">
-          <button type="button" onClick={HandlerTambah} className="grid grid-flow-col gap-2 items-center py-2 bg-red-500 rounded-lg">
+          <button
+            type="button"
+            onClick={HandlerTambah}
+            className="grid grid-flow-col gap-2 items-center py-2 bg-red-500 rounded-lg"
+          >
             <p>Batal</p>
           </button>
-          <button type="submit" className="grid grid-flow-col gap-2 items-center py-2 bg-secondary rounded-lg">
+          <button
+            type="submit"
+            className="grid grid-flow-col gap-2 items-center py-2 bg-secondary rounded-lg"
+          >
             <p>Simpan</p>
           </button>
         </div>
